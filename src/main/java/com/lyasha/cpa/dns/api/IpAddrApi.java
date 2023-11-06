aff9741c108832a3be3b2ab6950b97e88ed5bf4f
LazyYasha <watsnliu@outlook.com>
2023-11-06 20:36:48 +0800

    Add ip.3322.net IP provider
    will provide true IP rather than VPN IP in CN area

--- src/main/java/com/lyasha/cpa/dns/api/IpAddrApi.java
+++ src/main/java/com/lyasha/cpa/dns/api/IpAddrApi.java
@@ -1,14 +1,24 @@
 package com.lyasha.cpa.dns.api;
 
+import lombok.RequiredArgsConstructor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
+@RequiredArgsConstructor
 public class IpAddrApi {
 
     private static final OkHttpClient ipClient = new OkHttpClient();
+    private static final String CLOUDFLARE_PROVIDER = "Cloudflare";
+    private static final String IP3322_PROVIDER = "3322";
+
+    private final String ipProvider;
 
     public String getPublicIpv4Addr() {
-        return getIpv4AddrByCloudflareTraceAPI();
+        return switch (ipProvider) {
+            case IP3322_PROVIDER -> getIpv4AddrBy3322API();
+            case CLOUDFLARE_PROVIDER -> getIpv4AddrByCloudflareTraceAPI();
+            default -> getIpv4AddrByCloudflareTraceAPI();
+        };
     }
 
     protected static String getIpv4AddrByCloudflareTraceAPI() {
@@ -32,4 +42,18 @@ public class IpAddrApi {
         }
     }
 
+    protected static String getIpv4AddrBy3322API() {
+        var newRequest = new Request.Builder()
+                .url("https://ip.3322.net/")
+                .build();
+        try (var response = ipClient.newCall(newRequest).execute()) {
+            var body = response.body();
+            if (body == null) {
+                return null;
+            }
+            return body.string();
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
