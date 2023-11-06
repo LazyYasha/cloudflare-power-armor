5d4bf7ef44a0617c66ae4ee2f110ffeb0ccb0d20
LazyYasha <watsnliu@outlook.com>
2023-11-06 21:43:35 +0800

    Add configurations for calling cloudflare DNS API

---
+++ src/main/java/com/lyasha/cpa/dns/config/CpaDdnsProperties.java
@@ -0,0 +1,28 @@
+package com.lyasha.cpa.dns.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "cpa.ddns")
+@ConditionalOnProperty(name = "cpa.ddns.enabled", havingValue = "true")
+public class CpaDdnsProperties {
+
+    private boolean enabled;
+    private String ipProvider;
+    private List<DDnsTarget> targets;
+
+    @Data
+    public static class DDnsTarget {
+        private String zoneId;
+        private String type;
+        private String name;
+        private boolean proxied;
+    }
+}
